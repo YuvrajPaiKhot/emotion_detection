e3f211755a8532ca99f01af4fba619a989003de9
Yuvraj Pai Khot <102202216+YuvrajPaiKhot@users.noreply.github.com>
2024-08-04 15:03:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+
+app = Flask(__name__)
+CORS(app)
+
+# Load the model
+model = joblib.load('text_emotion_model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    text = data['text']
+    
+    # Preprocess the text (if needed)
+    # prediction = model.predict([preprocessed_text])
+    prediction = model.predict([text])
+    
+    return jsonify({'emotion': prediction[0]})
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+# In[ ]:
+
+
+
+
