9fc55c6bad3785710e6cd4ac36dc1c953569b846
Yuvraj Pai Khot <102202216+YuvrajPaiKhot@users.noreply.github.com>
2024-08-04 15:54:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -20,4 +21,5 @@ def predict():
     return jsonify({'emotion': prediction[0]})
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
